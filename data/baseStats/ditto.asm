6f8de0b533bed85b5e04bcf6e2c0a22a4f170d0d
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-03-24 03:31:15 +0100

    Update ditto.asm

--- data/baseStats/ditto.asm
+++ data/baseStats/ditto.asm
@@ -1,9 +1,9 @@
 db DEX_DITTO ; pokedex id
-db 48 ; base hp
-db 48 ; base attack
-db 48 ; base defense
-db 48 ; base speed
-db 48 ; base special
+db 58 ; base hp
+db 58 ; base attack
+db 58 ; base defense
+db 58 ; base speed
+db 58 ; base special
 db NORMAL ; species type 1
 db NORMAL ; species type 2
 db 35 ; catch rate
