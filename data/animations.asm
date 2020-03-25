551c3edb4b11d543b5340962fdd48c5c76ae3e19
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-03-25 18:03:19 +0100

    Update animations.asm

--- data/animations.asm
+++ data/animations.asm
@@ -163,6 +163,7 @@ AttackAnimationPointers:
 	dw SuperFangAnim
 	dw SlashAnim
 	dw SubstituteAnim
+	dw TwisterAnim
 	dw StruggleAnim
 	dw ShowPicAnim
 	dw EnemyFlashAnim
@@ -1127,6 +1128,14 @@ SubstituteAnim:
 	db SE_SLIDE_MON_OFF, $A3
 	db $08,$FF,$47
 	db SE_SUBSTITUTE_MON, $FF
+	db $FF
+
+TwisterAnim:
+	db SE_DARK_SCREEN_PALETTE, $FF
+	db $46,$A4,$10
+	db SE_DARK_SCREEN_FLASH, $FF
+	db $46,$FF,$04
+	db SE_RESET_SCREEN_PALETTE, $FF
 	db $FF
 
 BallTossAnim:
