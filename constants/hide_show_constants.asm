7b6aa10e2d05a9be79f32a235ff2cdd91585c735
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-03-25 02:52:06 +0100

    Update hide_show_constants.asm

--- constants/hide_show_constants.asm
+++ constants/hide_show_constants.asm
@@ -192,7 +192,7 @@ const_value = 0
 	const HS_SILPH_CO_11F_1                ; B7
 	const HS_SILPH_CO_11F_2                ; B8
 	const HS_SILPH_CO_11F_3                ; B9
-	const HS_UNUSED_MAP_F4_1               ; BA XXX sprite doesn't exist
+	const HS_MEW                           ; BA XXX sprite doesn't exist ; Mew Swap
 	const HS_POKEMON_MANSION_2F_ITEM       ; BB X
 	const HS_POKEMON_MANSION_3F_ITEM_1     ; BC X
 	const HS_POKEMON_MANSION_3F_ITEM_2     ; BD X
