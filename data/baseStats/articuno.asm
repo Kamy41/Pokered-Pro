5eb1c9fe9bf37e87024232d0cc4979c422923f7c
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-06 03:56:03 +0200

    Update articuno.asm

--- data/baseStats/articuno.asm
+++ data/baseStats/articuno.asm
@@ -2,7 +2,7 @@ db DEX_ARTICUNO ; pokedex id
 db 90 ; base hp
 db 85 ; base attack
 db 100 ; base defense
-db 85 ; base speed
+db 90 ; base speed
 db 125 ; base special
 db ICE ; species type 1
 db FLYING ; species type 2
