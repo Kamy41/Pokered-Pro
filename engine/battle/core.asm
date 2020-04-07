85f7ae2e9d94ba62b6729b606f2bf9c443c286f9
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-08 00:52:59 +0200

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5180,14 +5180,8 @@ HandleBuildingRage:
 	ld hl, BuildingRageText
 	call PrintText
 	call StatModifierUpEffect ; stat modifier raising function	
-	pop hl
+	call DisplayBattleMenu
 	xor a
-	ldd [hl], a ; null move effect
-	ld a, RAGE
-	ld [hl], a ; restore the target pokemon's move number to Rage
-	ld a, [H_WHOSETURN]
-	xor $01 ; flip turn back to the way it was
-	ld [H_WHOSETURN], a
 	ret
 	
 BuildingRageText:
