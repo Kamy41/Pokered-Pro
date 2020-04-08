c15ebc39118360e2881c56683dfbdc3213654e8e
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-08 14:04:54 +0200

    Update mewtwo.asm

--- data/baseStats/mewtwo.asm
+++ data/baseStats/mewtwo.asm
@@ -22,7 +22,7 @@ db 5 ; growth rate
 	tmlearn 9,10,11,12,13,14,15,16
 	tmlearn 17,18,19,20,22,24
 	tmlearn 25,29,30,31,32
-	tmlearn 33,34,35,36,38,40
+	tmlearn 33,34,35,36,38,39,40
 	tmlearn 44,45,46
 	tmlearn 49,50,54,55
 db 0 ; padding
