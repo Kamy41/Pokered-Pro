9f935c5611f547335cdea8d2f0026d35a00a53d2
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-09 01:10:24 +0200

    Update type_names.asm

--- text/type_names.asm
+++ text/type_names.asm
@@ -42,7 +42,7 @@ TypeNames:
 .Ice:      db "ICE@"
 .Ground:   db "GROUND@"
 .Rock:     db "ROCK@"
-.Bird:     db "BIRD@"
+.Bird:     db "UNKNOWN@"
 .Bug:      db "BUG@"
 .Ghost:    db "GHOST@"
 .Dragon:   db "DRAGON@"
