818f76e1a7c854c2f709dbf3d83643be7a2164db
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-10 14:32:44 +0200

    Update trainer_ai.asm

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -278,7 +278,7 @@ ReadMove:
 TrainerClassMoveChoiceModifications:
 	db 0      ; YOUNGSTER
 	db 1,0    ; BUG CATCHER
-	db 1,0    ; LASS
+	db 1,3,0  ; LASS
 	db 1,3,0  ; SAILOR
 	db 1,0    ; JR_TRAINER_M
 	db 1,0    ; JR_TRAINER_F
@@ -287,7 +287,7 @@ TrainerClassMoveChoiceModifications:
 	db 1,0    ; HIKER
 	db 1,0    ; BIKER
 	db 1,3,0  ; BURGLAR
-	db 1,0    ; ENGINEER
+	db 1,3,0  ; ENGINEER
 	db 1,2,0  ; JUGGLER_X
 	db 1,3,0  ; FISHER
 	db 1,3,0  ; SWIMMER
@@ -308,20 +308,20 @@ TrainerClassMoveChoiceModifications:
 	db 1,0    ; ROCKET
 	db 1,3,0  ; COOLTRAINER_M
 	db 1,3,0  ; COOLTRAINER_F
-	db 1,0    ; BRUNO
-	db 1,0    ; BROCK
+	db 1,3,0  ; BRUNO
+	db 1,3,0  ; BROCK
 	db 1,3,0  ; MISTY
 	db 1,3,0  ; LT_SURGE
 	db 1,3,0  ; ERIKA
 	db 1,3,0  ; KOGA
 	db 1,3,0  ; BLAINE
 	db 1,3,0  ; SABRINA
-	db 1,2,0  ; GENTLEMAN
+	db 1,2,3,0; GENTLEMAN
 	db 1,3,0  ; SONY2
 	db 1,3,0  ; SONY3
 	db 1,2,3,0; LORELEI
 	db 1,0    ; CHANNELER
-	db 1,0    ; AGATHA
+	db 1,3,0  ; AGATHA
 	db 1,3,0  ; LANCE
 
 INCLUDE "engine/battle/trainer_pic_money_pointers.asm"
@@ -433,7 +433,7 @@ BlackbeltAI:
 GiovanniAI:
 	cp 25 percent + 1
 	ret nc
-	jp AIUseGuardSpec
+	jp AIUseXAccuracy
 
 CooltrainerMAI:
 	cp 25 percent + 1
@@ -473,7 +473,7 @@ ErikaAI:
 	ld a, 10
 	call AICheckIfHPBelowFraction
 	ret nc
-	jp AIUsePotion
+	jp AIUseSuperPotion
 
 KogaAI:
 	cp 25 percent + 1
@@ -583,7 +583,7 @@ AIUseFullRestore:
 	ld [wEnemyMonHP], a
 	jr AIPrintItemUseAndUpdateHPBar
 
-AIUsePotion:
+AIUsePotion: ; unused
 ; enemy trainer heals his monster with a potion
 	ld a, POTION
 	ld b, 20
@@ -734,14 +734,14 @@ AICureStatus:
 	res 0, [hl]
 	ret
 
-AIUseXAccuracy: ; unused
+AIUseXAccuracy:
 	call AIPlayRestoringSFX
 	ld hl, wEnemyBattleStatus2
 	set 0, [hl]
 	ld a, X_ACCURACY
 	jp AIPrintItemUse
 
-AIUseGuardSpec:
+AIUseGuardSpec: ; unused
 	call AIPlayRestoringSFX
 	ld hl, wEnemyBattleStatus2
 	set 1, [hl]
