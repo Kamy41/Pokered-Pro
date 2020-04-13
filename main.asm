2b5ab72e3607b2793128aaa4c421d9f7236357d5
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-13 11:26:40 +0200

    Revert "Update main.asm"
    This reverts commit 9849f589247b7db8baec1fdfee112f0ae60186d3.

--- main.asm
+++ main.asm
@@ -595,6 +595,8 @@ GrowlithePicFront::   INCBIN "pic/bmon/growlithe.pic"
 GrowlithePicBack::    INCBIN "pic/monback/growlitheb.pic"
 OnixPicFront::        INCBIN "pic/bmon/onix.pic"
 OnixPicBack::         INCBIN "pic/monback/onixb.pic"
+FearowPicFront::      INCBIN "pic/bmon/fearow.pic"
+FearowPicBack::       INCBIN "pic/monback/fearowb.pic"
 PidgeyPicFront::      INCBIN "pic/bmon/pidgey.pic"
 PidgeyPicBack::       INCBIN "pic/monback/pidgeyb.pic"
 SlowpokePicFront::    INCBIN "pic/bmon/slowpoke.pic"
@@ -877,8 +879,7 @@ WeepinbellPicFront::   INCBIN "pic/bmon/weepinbell.pic"
 WeepinbellPicBack::    INCBIN "pic/monback/weepinbellb.pic"
 VictreebelPicFront::   INCBIN "pic/bmon/victreebel.pic"
 VictreebelPicBack::    INCBIN "pic/monback/victreebelb.pic"
-FearowPicFront::       INCBIN "pic/bmon/fearow.pic"
-FearowPicBack::        INCBIN "pic/monback/fearowb.pic"
+
 
 SECTION "Battle (BANK D)", ROMX
 
