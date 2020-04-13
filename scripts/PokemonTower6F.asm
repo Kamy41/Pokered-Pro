6a8625585b82cefeb1819b622f6b131429767d1f
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2020-04-13 17:10:04 +0200

    Update PokemonTower6F.asm

--- scripts/PokemonTower6F.asm
+++ scripts/PokemonTower6F.asm
@@ -33,9 +33,9 @@ PokemonTower6Script0:
 	ld [hSpriteIndexOrTextID], a
 	call DisplayTextID
 ;joenote - set a bit to indicate this is a ghost marowak battle
-	ld a, [wUnusedD721]
+	ld a, [wUnusedD71F]
 	set 3, a
-	ld [wUnusedD721], a
+	ld [wUnusedD71F], a
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	ld a, MAROWAK
 	ld [wCurOpponent], a
