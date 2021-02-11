d2df640a5b622e8a90aca3d9043e154c85691ad3
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2021-02-12 00:18:14 +0100

    Update OaksLab.asm

--- text/maps/OaksLab.asm
+++ text/maps/OaksLab.asm
@@ -133,8 +133,8 @@ _OaksLabGivePokeballsText2::
 	cont "it's fair game."
 
 	para "Just throw a #"
-	line "BALL at it and try"
-	line "to catch it!"
+	line "BALL at it and"
+	cont "try to catch it!"
 
 	para "This won't always"
 	line "work, though."
@@ -146,7 +146,7 @@ _OaksLabGivePokeballsText2::
 
 _OaksLabPleaseVisitText::
 	text "OAK: Come see me"
-	line "sometimes."
+	line "sometime."
 
 	para "I want to know how"
 	line "your #DEX is"
