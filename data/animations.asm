0880ce0d55767c63c018af6dd3a3bcfe26eaad0c
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2021-07-07 23:11:30 +0200

    Update animations.asm

--- data/animations.asm
+++ data/animations.asm
@@ -460,7 +460,7 @@ SupersonicAnim:
 SonicBoomAnim:
 	db $46,$2D,$15
 	db $46,$2D,$15
-	db $06,$2F,$16
+	db $05,$2F,$31
 	db $46,$FF,$05
 	db $FF
 
@@ -1130,7 +1130,7 @@ SubstituteAnim:
 
 TwisterAnim:
 	db SE_DARK_SCREEN_PALETTE, $FF
-	db $46,$A4,$10
+	db $44,$A4,$10
 	db SE_DARK_SCREEN_FLASH, $FF
 	db $46,$FF,$04
 	db SE_RESET_SCREEN_PALETTE, $FF
