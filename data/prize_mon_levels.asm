f37a5f1505d65b833e55c4c1ed8182f6977f9432
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2021-07-25 18:37:56 +0200

    Update prize_mon_levels.asm

--- data/prize_mon_levels.asm
+++ data/prize_mon_levels.asm
@@ -1,7 +1,7 @@
 PrizeMonLevelDictionary:
 IF DEF(_RED)
 	db ABRA,9
-	db CLEFAIRY,8
+	db CLEFAIRY,12
 	db NIDORINA,17
 
 	db DRATINI,18
@@ -9,11 +9,11 @@ IF DEF(_RED)
 	db PORYGON,26
 ENDC
 IF DEF(_BLUE)
-	db ABRA,6
+	db ABRA,9
 	db CLEFAIRY,12
 	db NIDORINO,17
 
-	db PINSIR,20
-	db DRATINI,24
-	db PORYGON,18
+	db DRATINI,18
+	db PINSIR,25
+	db PORYGON,26
 ENDC
