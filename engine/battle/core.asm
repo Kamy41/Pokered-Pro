6efd58cef253c70f7f66efc2b016bbad3cedb217
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2021-10-22 02:02:20 +0200

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -152,16 +152,11 @@ SlidePlayerAndEnemySilhouettesOnScreen:
 	ld c, $90
 	ld a, c
 	ld [hSCX], a
-	call DelayFrame	
-	; ld a, %11100100 ; inverted palette for silhouette effect
-	; ld [rBGP], a
+	call DelayFrame
 	ld a, %11111100 ; make the mon a black silhouette
 	ld [rBGP], a
-	; ld [rOBP0], a
-	; ld [rOBP1], a
-	ld b, SET_PAL_BATTLE
-	call RunPaletteCommand
-	
+	ld [rOBP0], a
+	ld [rOBP1], a	
 .slideSilhouettesLoop ; slide silhouettes of the player's pic and the enemy's pic onto the screen
 	ld h, b
 	ld l, $40
