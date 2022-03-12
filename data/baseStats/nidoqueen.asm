1c0a0423d4aab6e7821020cb989a227f851b1829
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-03-12 01:06:41 +0100

    Update nidoqueen.asm

--- data/baseStats/nidoqueen.asm
+++ data/baseStats/nidoqueen.asm
@@ -14,8 +14,8 @@ dw NidoqueenPicBack
 ; attacks known at lvl 0
 db TACKLE
 db SCRATCH
-db TAIL_WHIP
-db BODY_SLAM
+db DOUBLE_KICK
+db POISON_STING
 db 3 ; growth rate
 ; learnset
 	tmlearn 1,5,6,7,8
