14fbc975e904d75a0df45e61ca4333bb9e8413d3
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-03-12 01:00:03 +0100

    Update nidoranf.asm

--- data/baseStats/nidoranf.asm
+++ data/baseStats/nidoranf.asm
@@ -12,8 +12,8 @@ INCBIN "pic/bmon/nidoranf.pic",0,1 ; 55, sprite dimensions
 dw NidoranFPicFront
 dw NidoranFPicBack
 ; attacks known at lvl 0
-db GROWL
 db TACKLE
+db GROWL
 db 0
 db 0
 db 3 ; growth rate
