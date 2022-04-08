c4535258b65ab59dd53ec5dd18339753a917c56d
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-04-08 02:16:56 +0200

    Update moves.asm

--- data/moves.asm
+++ data/moves.asm
@@ -176,4 +176,4 @@ MoveEnd:
 	move SLASH,        NO_ADDITIONAL_EFFECT,        70, NORMAL,   100, 20
 	move SUBSTITUTE,   SUBSTITUTE_EFFECT,            0, NORMAL,   100, 10
 	move TWISTER,      FLINCH_SIDE_EFFECT2,         40, DRAGON,   100, 20
-	move STRUGGLE,     RECOIL_EFFECT,               50, BIRD,     100, 30
+	move STRUGGLE,     RECOIL_EFFECT,               50, NORMAL,   100, 30
