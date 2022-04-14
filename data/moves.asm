3b3d2e5668eb5fbb5f24bc11f8917a120e8eb147
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-04-14 15:23:18 +0200

    Update moves.asm

--- data/moves.asm
+++ data/moves.asm
@@ -27,9 +27,9 @@ MoveEnd:
 	move SWORDS_DANCE, ATTACK_UP2_EFFECT,            0, NORMAL,   100, 20
 	move CUT,          NO_ADDITIONAL_EFFECT,        50, NORMAL,    95, 30
 	move GUST,         NO_ADDITIONAL_EFFECT,        40, FLYING,   100, 35
-	move WING_ATTACK,  NO_ADDITIONAL_EFFECT,        60, FLYING,   100, 35
+	move WING_ATTACK,  NO_ADDITIONAL_EFFECT,        50, FLYING,   100, 35
 	move WHIRLWIND,    SWITCH_AND_TELEPORT_EFFECT,   0, NORMAL,   100, 20
-	move FLY,          FLY_EFFECT,                  90, FLYING,    95, 15
+	move FLY,          FLY_EFFECT,                  70, FLYING,    95, 15
 	move BIND,         TRAPPING_EFFECT,             15, NORMAL,    85, 20
 	move SLAM,         NO_ADDITIONAL_EFFECT,        80, NORMAL,    75, 20
 	move VINE_WHIP,    NO_ADDITIONAL_EFFECT,        35, GRASS,    100, 15
