5c91ae911d84485207a8128e45293c3f3a5da2be
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-10-07 10:49:36 +0200

    Update palettes.asm

--- engine/palettes.asm
+++ engine/palettes.asm
@@ -269,9 +269,9 @@ BadgeBlkDataLengths:
 	db 6     ; Earth Badge
 
 DeterminePaletteID:
-	bit TRANSFORMED, a ; a is battle status 3
-	ld a, DEX_DITTO	;ld a, PAL_GREYMON  ; if the mon has used Transform, use Ditto's palette
-	jr nz, DeterminePaletteIDOutOfBattle.skipDexNumConversion ;ret nz
+	;bit TRANSFORMED, a ; a is battle status 3
+	;ld a, PAL_GREYMON  ; if the mon has used Transform, use Ditto's palette
+	;ret nz
 	ld a, [hl]
 DeterminePaletteIDOutOfBattle:
 	ld [wd11e], a
