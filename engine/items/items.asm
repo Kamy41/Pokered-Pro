3fd1db6fd687bf0c5549b6ec0833b1c7f9ec46b6
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2022-10-07 10:22:22 +0200

    Update items.asm

--- engine/items/items.asm
+++ engine/items/items.asm
@@ -473,13 +473,9 @@ ItemUseBall:
 	ld hl, wEnemyBattleStatus3
 	bit TRANSFORMED, [hl]
 	jr z, .notTransformed
-	ld a, [wCurMap] ; check which map we're on
-	cp VERMILION_DOCK ; If we're on vermiliondock, the only Pokemon that can transform is MEW
-	ld a, MEW
-	jr z, .vermiliondock ; if we're on vermiliondock, we're done. Otherwise, load DITTO instead
-	ld a, DITTO
-.vermiliondock
-	ld [wEnemyMonSpecies2], a
+;joenote - these two lines removed to fix the ditto bug
+	;ld a, DITTO
+	;ld [wEnemyMonSpecies2], a
 	jr .skip6
 
 .notTransformed
