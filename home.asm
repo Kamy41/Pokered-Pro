29a11e2f60047294a51b538992979429b8887044
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2023-12-16 04:26:53 +0100

    Update home.asm

--- home.asm
+++ home.asm
@@ -282,7 +282,7 @@ LoadFrontSpriteByMonIndex::
 	cp NUM_POKEMON + 1
 	jr c, .validDexNumber   ; dex >#151 invalid
 .invalidDexNumber
-	ld a, RHYDON ; $1
+;	ld a, RHYDON ; $1
 ;	ld [wcf91], a
 	ret
 .validDexNumber        
