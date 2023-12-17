31978fd97ce4be9531a0ac74f9f636789f22115f
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2023-12-17 03:44:26 +0100

    Update trainer_ai.asm

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -214,9 +214,8 @@ AIMoveChoiceModification3:
 	jr c, .notEffectiveMove
 	ld a, [wEnemyMovePower]  ; added for BP check
 	and a 	                 ; check if it's zero
-	jr z, .notEffectiveMove  ; added for BP check
-	nop
-	nop
+	jr c, .notEffectiveMove  ; added for BP check	
+	jr z, .nextMove		 ; added for BP check
 	dec [hl] ; slightly encourage this move
 	jr .nextMove
 .notEffectiveMove ; discourages non-effective moves if better moves are available	
