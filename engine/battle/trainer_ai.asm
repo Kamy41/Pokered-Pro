bb6493ee39dd19db1c3bb4db72c2ebe0a08d46bc
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2023-12-17 19:44:43 +0100

    Update trainer_ai.asm

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -216,8 +216,8 @@ AIMoveChoiceModification3:
 	and a 	                 ; check if it's zero
 	jr z, .nextMove		 ; added for BP check
 	dec [hl] ; slightly encourage this move
-        nop	                 ; added to preserve Missingno
-	nop	                 ; added to preserve Missingno
+;       nop	                 ; added to preserve Missingno
+;	nop	                 ; added to preserve Missingno
 	jr .nextMove
 .notEffectiveMove ; discourages non-effective moves if better moves are available	
 	push hl
