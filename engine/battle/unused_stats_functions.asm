93fbaab122c6e1caf1aadab5e695691da1e76788
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2023-12-17 23:43:33 +0100

    Update unused_stats_functions.asm

--- engine/battle/unused_stats_functions.asm
+++ engine/battle/unused_stats_functions.asm
@@ -1,32 +1,59 @@
-DoubleSelectedStats:	
-	ret
-HalveSelectedStats:	
-	ret
-UndoBurnParStats:
-	ld hl, wBattleMonStatus
-	ld de, wPlayerStatsToDouble
+; does nothing since no stats are ever selected (barring glitches)
+DoubleSelectedStats:
 	ld a, [H_WHOSETURN]
 	and a
-	jr z, .checkburn
-	ld hl, wEnemyMonStatus
-	ld de, wEnemyStatsToDouble
-.checkburn
-	ld a, [hl]		;load statuses
-	and 1 << BRN	;test for burn 
-	jr z, .checkpar
-	ld a, $01
-	ld [de], a	;set attack to be doubled to undo the stat change of BRN
-	call DoubleSelectedStats
-	jr .return
-.checkpar
-	ld a, [hl]		;load statuses
-	and 1 << PAR	;test for paralyze 
-	jr z, .return
-	ld a, $04
-	ld [de], a	;set speed to be doubled (done twice) to undo the stat change of PAR
-	call DoubleSelectedStats
-	call DoubleSelectedStats
-.return
-	xor a
-	ld [de], a	;reset the stat change bits
+	ld a, [wPlayerStatsToDouble]
+	ld hl, wBattleMonAttack + 1
+	jr z, .notEnemyTurn
+	ld a, [wEnemyStatsToDouble]
+	ld hl, wEnemyMonAttack + 1
+.notEnemyTurn
+	ld c, 4
+	ld b, a
+.loop
+	srl b
+	call c, .doubleStat
+	inc hl
+	inc hl
+	dec c
+	ret z
+	jr .loop
+.doubleStat
+	ld a, [hl]
+	add a
+	ld [hld], a
+	ld a, [hl]
+	rl a
+	ld [hli], a
+	ret
+; does nothing since no stats are ever selected (barring glitches)
+HalveSelectedStats:
+	ld a, [H_WHOSETURN]
+	and a
+	ld a, [wPlayerStatsToHalve]
+	ld hl, wBattleMonAttack
+	jr z, .notEnemyTurn
+	ld a, [wEnemyStatsToHalve]
+	ld hl, wEnemyMonAttack
+.notEnemyTurn
+	ld c, 4
+	ld b, a
+.loop
+	srl b
+	call c, .halveStat
+	inc hl
+	inc hl
+	dec c
+	ret z
+	jr .loop
+.halveStat
+	ld a, [hl]
+	srl a
+	ld [hli], a
+	rr [hl]
+	or [hl]
+	jr nz, .nonzeroStat
+	ld [hl], 1
+.nonzeroStat
+	dec hl
 	ret
