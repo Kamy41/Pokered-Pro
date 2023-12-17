4fe43954b7a36587be4bec10038d2ab622f881e3
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2023-12-17 05:10:55 +0100

    Update stats_functions.asm

--- engine/battle/unused_stats_functions.asm
+++ engine/battle/unused_stats_functions.asm
@@ -60,3 +60,36 @@ HalveSelectedStats:
 .nonzeroStat
 	dec hl
 	ret
+
+;joenote - this function checks to see if a pkmn is paralyzed or burned
+;then it doubles attack if burned or quadruples speed if paralyzed.
+;It's meant to be run right before healing paralysis or burn so as to 
+;undo the stat changes.
+UndoBurnParStats:
+	ld hl, wBattleMonStatus
+	ld de, wPlayerStatsToDouble
+	ld a, [H_WHOSETURN]
+	and a
+	jr z, .checkburn
+	ld hl, wEnemyMonStatus
+	ld de, wEnemyStatsToDouble
+.checkburn
+	ld a, [hl]		;load statuses
+	and 1 << BRN	;test for burn 
+	jr z, .checkpar
+	ld a, $01
+	ld [de], a	;set attack to be doubled to undo the stat change of BRN
+	call DoubleSelectedStats
+	jr .return
+.checkpar
+	ld a, [hl]		;load statuses
+	and 1 << PAR	;test for paralyze 
+	jr z, .return
+	ld a, $04
+	ld [de], a	;set speed to be doubled (done twice) to undo the stat change of BRN
+	call DoubleSelectedStats
+	call DoubleSelectedStats
+.return
+	xor a
+	ld [de], a	;reset the stat change bits
+	ret
