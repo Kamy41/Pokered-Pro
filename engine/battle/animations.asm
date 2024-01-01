c888ea851da539cd969137a1d5f310bf945e8da4
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-01-01 22:59:43 +0100

    Update animations.asm

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -519,31 +519,32 @@ AnimationShakeScreenHorizontallySlow:
 	ret
 
 SetAnimationPalette:
+	ld b, $e4
 	ld a, [wOnSGB]
 	and a
-	ld a, $e4
+	;ld a, $e4	;redundant
 	jr z, .notSGB
-	ld a, $f0
-	ld [wAnimPalette], a
-	ld b, $e4
+	;ld a, $f0
+	;ld [wAnimPalette], a	;will handle in a SetAttackAnimPal
+
+	;if animation is TRADE_BALL_DROP_ANIM to TRADE_BALL_POOF_ANIM, load f0 into rOBP0
 	ld a, [wAnimationID]
 	cp TRADE_BALL_DROP_ANIM
 	jr c, .next
 	cp TRADE_BALL_POOF_ANIM + 1
 	jr nc, .next
-	ld b, $f0
+	ld b, $f0	
 .next
-	ld a, b
-	ld [rOBP0], a	
-	ld a, $6c
-	ld [rOBP1], a
-	ret
 .notSGB
-	ld a, $e4
-	ld [wAnimPalette], a
+	ld a, b
+;	ld a, $e4
+;	ld [wAnimPalette], a	;will handle in SetAttackAnimPal
 	ld [rOBP0], a
 	ld a, $6c
 	ld [rOBP1], a
+;	call UpdateGBCPal_OBP0
+;	call UpdateGBCPal_OBP1
+	call SetAttackAnimPal	;joenote - new function to handle animation palettes
 	ret
 
 PlaySubanimation:
@@ -724,6 +725,140 @@ AnimationIdSpecialEffects:
 	dw DoBallTossSpecialEffects
 
 	db $FF ; terminator
+
+SetAttackAnimPal:
+	call GetPredefRegisters
+
+	;set wAnimPalette based on if in grayscale or color
+	ld a, $e4
+	ld [wAnimPalette], a
+	ld a, [wOnSGB]
+	and a
+	ret z
+	ld a, $f0
+	ld [wAnimPalette], a
+	
+	;return if not on a GBC
+;	ld a, [hGBC]
+;	and a
+;	ret z 
+	
+	ld a, [wIsInBattle]
+	and a
+	ret z
+
+	;only continue for valid move animations
+	ld a, [wAnimationID]
+	and a
+	ret z
+	cp STRUGGLE		;check for non-move battle animations
+;	call nc, CheckIfBall
+	jp z, SetAttackAnimPal_otheranim	;reset battle pals for any other battle animations
+
+;doing a move animation, so find its type and apply color
+	ld a, [H_WHOSETURN]
+	and a
+	ld hl, wPlayerMoveType
+	jr z, .playermove
+	ld hl, wEnemyMoveType
+.playermove
+	ld a, [hl]
+	ld bc, $0000
+	ld c, a
+	ld hl, TypePalColorList
+	add hl, bc
+	ld a, [hl]
+	ld b, a
+	ret            ; ADDED TO CLOSE THE FUNCTION BEFORE THE JOJOBEAR'S ONE 
+
+;This function copies BGP colors 0-3 into OBP colors 0-3
+;It is meant to reset the object palettes on the fly
+SetAttackAnimPal_otheranim:
+	push hl
+	push bc
+	push de
+	
+	ld c, 4
+.loop
+	ld a, 4
+	sub c
+	;multiply index by 8 since each index represents 8 bytes worth of data
+	add a
+	add a
+	add a
+	ld [rBGPI], a
+	or $80 ; set auto-increment bit for writing
+	ld [rOBPI], a
+	ld hl, rBGPD
+	ld de, rOBPD
+	
+	ld b, 4
+.loop2
+	ld a, [rLCDC]
+	and rLCDC_ENABLE_MASK
+	jr z, .lcd_dis
+	;lcd in enabled otherwise
+.wait1
+	;wait for current blank period to end
+	ld a, [rSTAT]
+	and %10 ; mask for non-V-blank/non-H-blank STAT mode
+	jr z, .wait1
+	;out of blank period now
+.wait2
+	ld a, [rSTAT]
+	and %10 ; mask for non-V-blank/non-H-blank STAT mode
+	jr nz, .wait2
+	;back in blank period now
+.lcd_dis	
+	;LCD is disabled, so safe to read/write colors directly
+	ld a, [hl]
+	ld [de], a
+	ld a, [rBGPI]
+	inc a
+	ld [rBGPI], a
+	ld a, [hl]
+	ld [de], a
+	ld a, [rBGPI]
+	inc a
+	ld [rBGPI], a
+	dec b
+	jr nz, .loop2
+	
+	dec c
+	jr nz, .loop
+	
+	pop de
+	pop bc
+	pop hl
+	ret
+TypePalColorList:
+	db PAL_GREYMON;normal
+	db PAL_GREYMON;fighting
+	db PAL_CYANMON;flying
+	db PAL_PURPLEMON;poison
+	db PAL_BROWNMON;ground
+	db PAL_GREYMON;rock
+	db PAL_BLACK;untyped/bird
+	db PAL_GREENMON;bug
+	db PAL_PURPLEMON;ghost
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_BLACK;unused
+	db PAL_REDMON;fire
+	db PAL_BLUEMON;water
+	db PAL_GREENMON;grass
+	db PAL_YELLOWMON;electric
+	db PAL_PINKMON;psychic
+	db PAL_CYANMON;ice
+	db PAL_BLUEMON;dragon
 
 DoBallTossSpecialEffects:
 	ld a, [wcf91]
