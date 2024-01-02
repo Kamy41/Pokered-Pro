a71fcc73ee925816639758947dbb1efccde0e72e
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-01-02 23:53:08 +0100

    Update animations_pal.asm

--- engine/battle/animations_pal.asm
+++ engine/battle/animations_pal.asm
@@ -1,17 +1,10 @@
 SetAttackAnimPal:
 	call GetPredefRegisters
-
-	;set wAnimPalette based on if in grayscale or color
-	ld a, $e4
-	ld [wAnimPalette], a
-	ld a, [wOnSGB]
-	and a
-	ret z
+	
 	ld a, $f0
 	ld [wAnimPalette], a
 	
-	;return if not on a GBC
-	ld a, [wGBC]
+	ld a, [hGBC]
 	and a
 	ret z 
 	
@@ -23,10 +16,18 @@ SetAttackAnimPal:
 	ld a, [wAnimationID]
 	and a
 	ret z
-	cp STRUGGLE		;check for non-move battle animations
-;	call nc, CheckIfBall
-	jp z, SetAttackAnimPal_otheranim	;reset battle pals for any other battle animations
-
+	cp STRUGGLE
+	ret nc	
+	
+	ld a, $e4
+	ld [wAnimPalette], a
+	
+	push hl
+	push bc
+	push de
+	ld a, [wcf91]
+	push af
+	
 ;doing a move animation, so find its type and apply color
 	ld a, [H_WHOSETURN]
 	and a
@@ -41,68 +42,27 @@ SetAttackAnimPal:
 	add hl, bc
 	ld a, [hl]
 	ld b, a
-;	ret            ; ADDED TO CLOSE THE FUNCTION BEFORE THE JOJOBEAR'S ONE 
 
-;This function copies BGP colors 0-3 into OBP colors 0-3
-;It is meant to reset the object palettes on the fly
-SetAttackAnimPal_otheranim:
-	push hl
-	push bc
-	push de
-	
 	ld c, 4
-.loop
-	ld a, 4
-	sub c
-	;multiply index by 8 since each index represents 8 bytes worth of data
-	add a
-	add a
-	add a
-	ld [rBGPI], a
-	or $80 ; set auto-increment bit for writing
-	ld [rOBPI], a
-	ld hl, rBGPD
-	ld de, rOBPD
-	
-	ld b, 4
-.loop2
-	ld a, [rLCDC]
-	and rLCDC_ENABLE_MASK
-	jr z, .lcd_dis
-	;lcd in enabled otherwise
-.wait1
-	;wait for current blank period to end
-	ld a, [rSTAT]
-	and %10 ; mask for non-V-blank/non-H-blank STAT mode
-	jr z, .wait1
-	;out of blank period now
-.wait2
-	ld a, [rSTAT]
-	and %10 ; mask for non-V-blank/non-H-blank STAT mode
-	jr nz, .wait2
-	;back in blank period now
-.lcd_dis	
-	;LCD is disabled, so safe to read/write colors directly
-	ld a, [hl]
-	ld [de], a
-	ld a, [rBGPI]
-	inc a
-	ld [rBGPI], a
-	ld a, [hl]
-	ld [de], a
-	ld a, [rBGPI]
-	inc a
-	ld [rBGPI], a
-	dec b
-	jr nz, .loop2
-	
+.transfer
+	; ld d, CONVERT_OBP0
+	ld e, c
+	dec e
+	ld a, b	
+	add VICTREEBEL+1
+	ld [wcf91], a
+	push bc
+	callba TransferMonPal
+	pop bc
 	dec c
-	jr nz, .loop
+	jr nz, .transfer
 	
+	pop af
+	ld [wcf91], a
 	pop de
 	pop bc
 	pop hl
-	ret
+	ret	
 TypePalColorList:
 	db PAL_GREYMON;normal
 	db PAL_GREYMON;fighting
