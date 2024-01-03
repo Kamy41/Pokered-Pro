6bad9e5c930c95bddcb79d0da0543da2f8037754
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-01-03 01:39:42 +0100

    Update animations.asm

--- engine/battle/animations.asm
+++ engine/battle/animations.asm
@@ -523,7 +523,7 @@ SetAnimationPalette:
 	and a
 ;	ld a, $e4
 	jr z, .notSGB
-	ld a, $fa
+	ld a, $f0
 ;	ld a, PAL_MEWMON             ; new move pal
 	ld [wAnimPalette], a
 	ld b, $e4
@@ -532,7 +532,7 @@ SetAnimationPalette:
 	jr c, .next
 	cp TRADE_BALL_POOF_ANIM + 1
 	jr nc, .next
-	ld b, $fa
+	ld b, $f0
 .next
 	ld a, b
 	ld [rOBP0], a	
