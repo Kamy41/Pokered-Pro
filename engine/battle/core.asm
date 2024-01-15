13394d7cee5e7fa85addb6359029fac3809847dc
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-01-15 16:56:42 +0100

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4013,7 +4013,7 @@ PrintMoveFailureText:
 	ret nz
 
 	; if you get here, the mon used jump kick or hi jump kick and missed
-	; ld hl, wDamage ; since the move missed, wDamage will always contain 0 at this point.
+	ld hl, wDamage ; since the move missed, wDamage will always contain 0 at this point.
 	               ; Thus, recoil damage will always be equal to 1
 	               ; even if it was intended to be potential damage/8.
 	ld a, [hli]
@@ -4024,11 +4024,6 @@ PrintMoveFailureText:
 	rr b
 	srl a
 	rr b
-;;;;;
-	ld hl, wDamage
-	ld c, $1
-	add a, c
-;;;;;
 	ld [hl], b
 	dec hl
 	ld [hli], a
