7bb8328a3f7064481b252fb7cd22d52e4d8d02ac
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2024-06-12 17:17:16 +0200

    Update battle_16.asm

--- audio/sfx/battle_16.asm
+++ audio/sfx/battle_16.asm
@@ -1,5 +1,5 @@
 SFX_Battle_16_Ch8:
-	noise_note 1, 9, 4, 35
-	noise_note 1, 11, 4, 34
-	noise_note 8, 15, 1, 68
+	noise_note 2, 15, 4, 68
+	noise_note 2, 15, 4, 20
+	noise_note 15, 15, 1, 50
 	sound_ret
