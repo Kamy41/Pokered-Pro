890d55b8f854b898a95f7516da512d535f8cf0bf
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-03-21 13:53:18 +0100

    Update moves.asm

--- data/moves.asm
+++ data/moves.asm
@@ -23,7 +23,7 @@ MoveEnd:
 	move SCRATCH,      NO_ADDITIONAL_EFFECT,        40, NORMAL,   100, 35
 	move VICEGRIP,     NO_ADDITIONAL_EFFECT,        55, NORMAL,   100, 30
 	move GUILLOTINE,   OHKO_EFFECT,                  1, NORMAL,    30,  5
-	move RAZOR_WIND,   CHARGE_EFFECT,               80, NORMAL,   100, 10
+	move RAZOR_WIND,   CHARGE_EFFECT,               80, NORMAL,    80, 10
 	move SWORDS_DANCE, ATTACK_UP2_EFFECT,            0, NORMAL,   100, 20
 	move CUT,          NO_ADDITIONAL_EFFECT,        50, NORMAL,    95, 30
 	move GUST,         NO_ADDITIONAL_EFFECT,        40, FLYING,   100, 35
