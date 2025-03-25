db3ecb3268cc46236dbd22569e1491e32e0abe95
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-03-25 15:49:39 +0100

    Update trainer_ai.asm

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -159,7 +159,7 @@ AIMoveChoiceModification2:
 	ld a, [wAILayer2Encouragement]
 	and a 	;joenote - AI layer 2 should activate on 1st turn instead of 2nd turn after send
 ;       cp $0
-	ret nz
+	ret z
 	ld hl, wBuffer - 1 ; temp move selection array (-1 byte offset)
 	ld de, wEnemyMonMoves ; enemy moves
 	ld b, NUM_MOVES + 1
