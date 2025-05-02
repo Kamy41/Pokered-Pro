b26dd2ae43bc1ea57fee2c4a1fe996e843e8f024
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-05-03 00:35:59 +0200

    Update exeggutor.asm

--- data/baseStats/exeggutor.asm
+++ data/baseStats/exeggutor.asm
@@ -1,7 +1,7 @@
 db DEX_EXEGGUTOR ; pokedex id
 db 95 ; base hp
-db 95 ; base attack
-db 85 ; base defense
+db 85 ; base attack
+db 95 ; base defense
 db 55 ; base speed
 db 125 ; base special
 db GRASS ; species type 1
