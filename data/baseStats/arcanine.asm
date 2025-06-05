470fdf2a55f14d170618577cbdf8befca7eed9b5
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-06-06 00:55:46 +0200

    Update arcanine.asm

--- data/baseStats/arcanine.asm
+++ data/baseStats/arcanine.asm
@@ -12,7 +12,7 @@ INCBIN "pic/bmon/arcanine.pic",0,1 ; 77, sprite dimensions
 dw ArcaninePicFront
 dw ArcaninePicBack
 ; attacks known at lvl 0
-db AGILITY
+db BITE
 db LEER
 db FLAMETHROWER
 db TAKE_DOWN
