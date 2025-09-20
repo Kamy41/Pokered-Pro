174123b9cb16277941e5e1279542ccde9f16d3f7
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-09-20 18:25:23 +0200

    Update hitmonlee.asm

--- data/baseStats/hitmonlee.asm
+++ data/baseStats/hitmonlee.asm
@@ -1,9 +1,9 @@
 db DEX_HITMONLEE ; pokedex id
-db 50 ; base hp
+db 70 ; base hp
 db 120 ; base attack
 db 53 ; base defense
 db 87 ; base speed
-db 50 ; base special
+db 55 ; base special
 db FIGHTING ; species type 1
 db FIGHTING ; species type 2
 db 45 ; catch rate
