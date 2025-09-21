50115a46604b330a7347e9efae72c695ebdc8671
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-09-21 16:59:24 +0200

    Update evos_moves.asm

--- data/evos_moves.asm
+++ data/evos_moves.asm
@@ -658,7 +658,7 @@ MachokeEvosMoves:
 	db 0
 ; Learnset
 	db 20, LOW_KICK
-	db 25, LEER
+	db 25, MEDITATE
 	db 36, FOCUS_ENERGY
 	db 44, SEISMIC_TOSS
 	db 52, SUBMISSION
@@ -1305,7 +1305,7 @@ MachopEvosMoves:
 	db 0
 ; Learnset
 	db 20, LOW_KICK
-	db 25, LEER
+	db 25, MEDITATE
 	db 32, FOCUS_ENERGY
 	db 39, SEISMIC_TOSS
 	db 46, SUBMISSION
@@ -1504,7 +1504,7 @@ MachampEvosMoves:
 	db 0
 ; Learnset
 	db 20, LOW_KICK
-	db 25, LEER
+	db 25, MEDITATE
 	db 36, FOCUS_ENERGY
 	db 44, SEISMIC_TOSS
 	db 52, SUBMISSION
