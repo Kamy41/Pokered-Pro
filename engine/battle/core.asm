90f77d7e967c4353f5c73f8b5e27ae97e6c68cc0
Kamy41 <30781801+Kamy41@users.noreply.github.com>
2025-10-19 18:28:48 +0200

    Update core.asm

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -2697,7 +2697,7 @@ SelectMenuItem:
 	jr nz, .select
 ; MIMIC-specific code only
 ;;;;;;;;;; PureRGBnote: FIXED: bug with displaying the mimic menu's WHICH TECHNIQUE? text. The bottom line of text wasn't erased. It should be.
-	hlcoord 1, 14
+	coord hl 1, 14
 	lb bc, 3, 18
 	call ClearScreenArea
 ;;;;;;;;;;
